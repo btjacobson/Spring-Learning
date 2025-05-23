1f2f8319907e0ab5cb13b79ee0b64b79e6212eff
btjacobson <31464526+btjacobson@users.noreply.github.com>
2025-05-22 19:13:40 -0600

    Working on the todo app now

---
+++ todo-app/src/main/java/com/example/todo_app/models/Todo.java
@@ -0,0 +1,73 @@
+package com.example.todo_app.models;
+
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+
+@Component
+public class Todo {
+    private int id;
+    private String username;
+    private String description;
+    private LocalDate completeBy;
+    private boolean completed;
+
+    public Todo(int id, String username, String description, LocalDate completeBy, boolean completed) {
+        this.id = id;
+        this.username = username;
+        this.description = description;
+        this.completeBy = completeBy;
+        this.completed = completed;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDate getCompleteBy() {
+        return completeBy;
+    }
+
+    public void setCompleteBy(LocalDate completeBy) {
+        this.completeBy = completeBy;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+
+    @Override
+    public String toString() {
+        return "Todo{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", description='" + description + '\'' +
+                ", completeBy=" + completeBy +
+                ", completed=" + completed +
+                '}';
+    }
+}
---
+++ todo-app/src/main/java/com/example/todo_app/services/TodoService.java
@@ -0,0 +1,25 @@
+package com.example.todo_app.services;
+
+import com.example.todo_app.models.Todo;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Service
+public class TodoService {
+    private static List<Todo> todos;
+    static {
+        todos.add(new Todo(1, "test", "Finish this", LocalDate.now().plusYears(1), false));
+        todos.add(new Todo(1, "test", "Finish this 2", LocalDate.now().plusYears(1), false));
+        todos.add(new Todo(1, "test", "Finish this 3", LocalDate.now().plusYears(1), false));
+        todos.add(new Todo(1, "test", "Finish this 4", LocalDate.now().plusYears(1), false));
+        todos.add(new Todo(1, "test", "Finish this 5", LocalDate.now().plusYears(1), false));
+        todos.add(new Todo(1, "test", "Finish this 6", LocalDate.now().plusYears(1), false));
+        todos.add(new Todo(1, "test", "Finish this 7", LocalDate.now().plusYears(1), false));
+    }
+
+    public List<Todo> findTodosByUsername(String username) {
+        return todos;
+    }
+}
