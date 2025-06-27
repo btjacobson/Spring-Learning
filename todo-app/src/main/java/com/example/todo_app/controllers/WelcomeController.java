9501bfe096669efbc59540f5dcc59e762ae56c0c
btjacobson <31464526+btjacobson@users.noreply.github.com>
2025-06-26 19:10:25 -0600

    Using spring security

---
+++ todo-app/src/main/java/com/example/todo_app/controllers/WelcomeController.java
@@ -0,0 +1,16 @@
+package com.example.todo_app.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+@Controller
+@SessionAttributes("username")
+public class WelcomeController {
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String gotoWelcomePage(ModelMap model) {
+        return "welcome";
+    }
+}
